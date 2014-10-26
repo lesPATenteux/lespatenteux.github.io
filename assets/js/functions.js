062279990c58633ff60f2e79b16991ae0a0b900f
Zénon Roy <king-zed@hotmail.com>
2014-10-26 14:17:47 -0400

    Code clean up and added BackboneJS

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,7 +2,12 @@ $(function($){
    
     $("#_who").on("click", function(){
         alert("Ce contenu n'est pas disponible pour le moment. Merci de revenir plus tard");
-    })
+    });
+    
+    window.onload = function(){
+        fade('#jumbo', 200);
+        fade('#alert', 400);
+    };
     
 })(jQuery);
 
